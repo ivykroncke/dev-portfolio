4aa3171ec0b3a8d2c64e44f963c74292efdbae6c
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-27 09:32:13 -0500

    Added app wrapper instead of importing app.cs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
-import Nav from './Components/Nav';
 import Home from './Components/Home';
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+height: 100%;
+font-family: Lato, sans-serif;
+a {
+  color: inherit;
+  text-decoration: none;
+}
+`
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
+      <AppWrapper>
+        <Router>
+          <Switch>
             <Route exact path='/' component={Home} />
-        </Switch>
-      </Router>
-    );
+          </Switch>
+        </Router>
+      </AppWrapper>
+    )
   }
 }
 
