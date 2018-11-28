27f0bdc53b1567a8cf95634242d683ee23cc25c0
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-28 11:50:07 -0500

    About and Footer combined stylistically

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,10 @@ import styled from 'styled-components'
 import media from 'styled-media-query'
 
 const AboutContainer = styled.div`
-border-radius: 5rem;
-margin: 10vh;
+width: 100vw;
+background-color: rgba(148, 165, 174, .5);
+margin: 0;
+padding: 10vh 10vw 0 10vw;
 display: flex;
 flex-direction: column;
 justify-content: space-around;
@@ -14,7 +16,7 @@ align-items: center;
 const StyledImage = styled.div`
 height: 30vh;
 width: 30vh;
-margin: 10vh;
+margin: 5vh;
 background-image: url('/img/ivyworking.jpg');
 background-size: cover;
 background-position: right;
@@ -27,12 +29,17 @@ margin-left: 5vw;
 word-wrap: normal;
 `
 
+const StyledHThree = styled.h3`
+text-align: center;
+padding-bottom: 5vh;
+`
+
 export default class About extends Component {
   render() {
     return (
       <AboutContainer>
         <AboutTextContainer>
-          <h3>Ivy Kroncke</h3>
+          <StyledHThree>About Ivy Kroncke</StyledHThree>
           <p>is a full-stack web developer with a background in fine arts including painting and printmaking. After receiving her Bachelor's of Fine Art from the Lamar Dodd School of Art, she worked in the interior design and art nonprofit industries. Looking for a new challenge, Kroncke completed an immersive thirteen week course in Web Development from General Assembly.</p>
           <p>Kroncke brings her design talents to web projects, combining problem-solving skills in coding with an eye for color and composition.</p>
         </AboutTextContainer>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,11 +3,19 @@ import styled from 'styled-components'
 import { Icon } from 'semantic-ui-react'
 import media from 'styled-media-query'
 
+const FooterWrapper = styled.div`
+background-color: rgba(148, 165, 174, .5);
+width: 100vw;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
 const StyledFooter = styled.div`
-width: 90vw;
+width: 30vw;
 padding: 5vh 5vw;
 display: flex;
-justify-content: center;
+justify-content: space-around;
 align-items: center;
   ${media.lessThan('medium')`
     justify-content: center;
@@ -15,20 +23,21 @@ align-items: center;
 `
 
 const FooterIcon = styled(Icon)`
-  padding: 0 3vw 0 3vw;
+  /* padding: 0 3vw 0 3vw; */
   ${media.lessThan('medium')`
-  padding: 0 8vw 0 8vw;
   `}
 `
 
 export default class Footer extends Component {
   render() {
     return (
-      <StyledFooter>
-        <a href='https://github.com/ivykroncke/' target='blank'><FooterIcon name='github' size='large' /></a>
-        <a href='mailto:ivy.lane.kroncke@gmail.com'><FooterIcon name='mail' size='large' /></a>
-        <a href='https://www.linkedin.com/in/ivykroncke/' target='blank'><FooterIcon name='linkedin' size='large' /></a>
-      </StyledFooter>
+      <FooterWrapper>
+        <StyledFooter>
+          <a href='https://github.com/ivykroncke/' target='blank'><FooterIcon name='github' size='large' /></a>
+          <a href='mailto:ivy.lane.kroncke@gmail.com'><FooterIcon name='mail' size='large' /></a>
+          <a href='https://www.linkedin.com/in/ivykroncke/' target='blank'><FooterIcon name='linkedin' size='large' /></a>
+        </StyledFooter>
+      </FooterWrapper>
     )
   }
 }
